9bfbdaee75f81b6d507c707feeff38ed869d3b15
Edyta Labedzka <edlabedzka@gmail.com>
2024-05-27 23:03:46 +0200

    Fully working code

--- task1/parser.py
+++ task1/parser.py
@@ -26,17 +26,20 @@ class URLParser:
     def parse_url(self, url):
         query = urlparse(url).query
         query_dict = parse_qs(query)
-        url_parsed = [query_dict.get(url_part, '') for url_part in self.url_mapping.keys()]
-        print(url_parsed)
+        url_parsed = [query_dict.get(url_part, [''])[0] for url_part in self.url_mapping.keys()]
         return url_parsed
 
     def write_parsed_to_file(self):
-        with open(f"{self.output_file_name}.tsv", 'w') as file:
+        with open(f"{self.output_file_name}.tsv", 'w', newline='') as file:
             writer = csv.writer(file, delimiter='\t')
+            header = ["url"]
+            header.extend(self.url_mapping.values())
+            writer.writerow(header)
             for url in self.read_url_from_file():
-                url_parsed = self.parse_url(url)
+                url_parsed = [url]
+                url_parsed.extend(self.parse_url(url))
                 writer.writerow(url_parsed)
 
 if __name__ == "__main__":
-    url_parser = URLParser(r'task1\data\task1_input.tsv', r'task1\data\task1_solution.tsv')
+    url_parser = URLParser(r'task1\data\task1_input.tsv', r'task1\data\task1_solution')
     url_parser.write_parsed_to_file()
