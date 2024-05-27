56567b3749a2199023e7457fb67896313c9d83f4
Edyta Labedzka <edlabedzka@gmail.com>
2024-05-28 00:36:09 +0200

    Added simple test

--- task1/parser.py
+++ task1/parser.py
@@ -15,7 +15,7 @@ class URLParser:
             "a_v":"schema_version",
             "a_g_campaignid":"ad_campaign_id",
             "a_g_keyword":"ad_keyword",
-            "a_g_adgroupid":"ad_group_id",
+            "a_g_adgroupid":"ad_adgroup_id",
             "a_g_creative":"ad_creative"
         }
         if not os.path.exists('task1\logs'):
@@ -59,10 +59,39 @@ class URLParser:
                     url_parsed.extend(self.parse_url(url))
                     writer.writerow(url_parsed)
             except Exception as e:
-                message = "Parsing failed, please check the logs"
-                logging.error(f"{message}. \nError: {str(e)}")
-                raise Exception(message)
+                logging.error(f"Parsing URLs failed due to error: \n{str(e)}")
+                raise Exception("Parsing failed, please check the logs!")
+
+class TestURLParser:
+    def __init__(self, input_file, target_file, test_file_name):
+        self.parser = URLParser(input_file, test_file_name)
+        self.parser.write_parsed_to_file()
+        self.input_file = input_file
+        self.test_file = f"{test_file_name}.tsv"
+        self.target_file = target_file
+        if not os.path.exists('task1\logs'):
+            os.makedirs('task1\logs')
+        today = date.today()
+        logs_file_name = f"task1\logs\{today.strftime('%d_%m_%Y')}.log"
+        logs_format = '%(asctime)s - %(levelname)s - %(message)s'
+        logging.basicConfig(filename=logs_file_name, level=logging.INFO, format=logs_format)
+
+    def compare_files(self):
+        logging.info(f"Running parser test based on files \ninput: {self.input_file}, \ntarget: {self.target_file}, \nreturned by parser: {self.test_file} ")
+        with open(self.test_file, 'r') as test, open(self.target_file, 'r') as target:
+            reader1 = csv.reader(test, delimiter='\t')
+            reader2 = csv.reader(target, delimiter='\t')
+
+            for row1, row2 in zip(reader1, reader2):
+                try:
+                    assert row1 == row2, f"Provided files don't match! \nTest file values:{row1} \nTarget file values:{row2}"
+                except AssertionError as ae:
+                    logging.error(str(ae))
+                    raise Exception("Parser test failed!")
+            logging.info("Parser test passed!")
 
 if __name__ == "__main__":
-    url_parser = URLParser(r'task1\data\task1_input.tsv', r'task1\data\task1_solution')
-    url_parser.write_parsed_to_file()
+    #url_parser = URLParser(r'task1\data\task1_input.tsv', r'task1\data\task1_solution')
+    #url_parser.write_parsed_to_file()
+    url_parser_test = TestURLParser(r'task1\data\task1_input.tsv', r'task1\data\task1_output.tsv', r'task1\data\task1_solution')
+    url_parser_test.compare_files()
