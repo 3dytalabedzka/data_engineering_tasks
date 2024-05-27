ba213ca5e6ea8af87404b30f6425c78fa859d261
Edyta Labedzka <edlabedzka@gmail.com>
2024-05-27 22:15:26 +0200

    Adding data and class outline

---
+++ task1/parser.py
@@ -0,0 +1,13 @@
+
+class URLParser:
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    def read_url_from_file(self):
+        pass
+
+    def parse_url(self):
+        pass
+
+    def write_parsed_to_file(self):
+        pass
